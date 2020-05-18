f10b06e1da4a12dd6357e7163517060fa48c690d
Arnas Boruchovas <aboruchovas@gmail.com>
2020-05-18 13:34:58 +0100

    Moving files around

---
+++ app/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 500,
+    backgroundColor: '#ffffff',
+    show: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadFile('app/index.html')
+  mainWindow.setResizable(false)
+  mainWindow.setMaximizable(false)
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
